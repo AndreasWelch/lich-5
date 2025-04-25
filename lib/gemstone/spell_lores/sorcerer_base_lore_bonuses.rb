7f27d3f961b7592a950b407bd21abbd86ec1dd4f
Andreas Welch <AndreasWelch@users.noreply.github.com>
2025-04-25 22:47:07 +0200

    initial reformatting and rubocop

--- lib/gemstone/spell_lores/sorcerer_base_lore_bonuses.rb
+++ lib/gemstone/spell_lores/sorcerer_base_lore_bonuses.rb
@@ -1,35 +1,34 @@
-
 module Lich
-    module Gemstone
-        module SpellLoreBonuses
-            SpellLoreBonuses.table[:sorcerer_base] = {
-                000 => {
-                    "effect" => {
-                        :description        => "",
-                        :bonus_amount       => 0,
-                        :bonus_type         => "",
-                        :bonus_to           => "",
-                        :chart              => chart_function(),
-                        :duration           => 0,
-                        :lore_used          => Skills.type_of_lore,
-                        :repetions          => 999,
-                        :repeat_modifier    => 0
-                    }
-                },
-                001 => {
-                    "effect" => {
-                        :description        => "",
-                        :bonus_amount       => 0,
-                        :bonus_type         => "",
-                        :bonus_to           => "",
-                        :chart              => chart_function(),
-                        :duration           => 0,
-                        :lore_used          => Skills.type_of_lore,
-                        :repetions          => 999,
-                        :repeat_modifier    => 0
-                    }
-                }
-            }
-        end
+  module Gemstone
+    module SpellLoreBonuses
+      SpellLoreBonuses.table[:sorcerer_base] = {
+        000 => {
+          "effect" => {
+            :description     => "",
+            :bonus_amount    => 0,
+            :bonus_type      => "",
+            :bonus_to        => "",
+            :chart           => chart_function(),
+            :duration        => 0,
+            :lore_used       => Skills.type_of_lore,
+            :repetions       => 999,
+            :repeat_modifier => 0
+          }
+        },
+        001 => {
+          "effect" => {
+            :description     => "",
+            :bonus_amount    => 0,
+            :bonus_type      => "",
+            :bonus_to        => "",
+            :chart           => chart_function(),
+            :duration        => 0,
+            :lore_used       => Skills.type_of_lore,
+            :repetions       => 999,
+            :repeat_modifier => 0
+          }
+        }
+      }
     end
+  end
 end
--- lib/gemstone/spelllores.rb
+++ lib/gemstone/spelllores.rb
@@ -1,36 +1,34 @@
-
-
 # requireds template for a lore bonus
 # note that spells can have multiple effects
-#    GENERIC:                                                                <<-- the spell circle name
-#        000:                                                                <<-- the spell number/id
-#            "effect name"                                                   <<-- basic name of the effect
-#                :description: "described effect/bonus"                      <<-- describes the effect or bonus that is applied
-#                :chart: calculation_chart(value)                            <<-- chart/table to reference to determine bonus
-#                :bonus_amount: 0                                            <<-- the bonus added per step, based on chart
-#                :bonus_type: [percent, ability, units, time, ranks, bonus]  <<-- the type of bonus, i.e. units, percent, effect
-#                :bonus_to: UNSPECIFIED                                      <<-- what the bonus actually applies to
-#                :bonus_max: 0                                               <<-- maximum bonus capped at, even if theoretically more is possible, 0 is no cap
-#                :duration: UNSPECIFIED                                      <<-- duration of the bonus, if applicable, use 0 if not
-#                :lore_used: Skills.type_of_lore                             <<-- lore to calculate off, Skills.to_bonus(:type_of_lore) for bonus instead of ranks
-#                :repetions: 999                                             <<-- number of times the ability can repeat, use 999 for infinite
-#                :repeat_modifier: 0                                         <<-- % modifier of chance to recur bonus
+#  GENERIC:                                                                <<-- the spell circle name
+#    000:                                                                <<-- the spell number/id
+#      "effect name"                                                   <<-- basic name of the effect
+#        :description: "described effect/bonus"                      <<-- describes the effect or bonus that is applied
+#        :chart: calculation_chart(value)                            <<-- chart/table to reference to determine bonus
+#        :bonus_amount: 0                                            <<-- the bonus added per step, based on chart
+#        :bonus_type: [percent, ability, units, time, ranks, bonus]  <<-- the type of bonus, i.e. units, percent, effect
+#        :bonus_to: UNSPECIFIED                                      <<-- what the bonus actually applies to
+#        :bonus_max: 0                                               <<-- maximum bonus capped at, even if theoretically more is possible, 0 is no cap
+#        :duration: UNSPECIFIED                                      <<-- duration of the bonus, if applicable, use 0 if not
+#        :lore_used: Skills.type_of_lore                             <<-- lore to calculate off, Skills.to_bonus(:type_of_lore) for bonus instead of ranks
+#        :repetions: 999                                             <<-- number of times the ability can repeat, use 999 for infinite
+#        :repeat_modifier: 0                                         <<-- % modifier of chance to recur bonus
 
 =begin
-                000 => {
-                    "effect" => {
-                        :description        => "",
-                        :chart              => chart_function(),
-                        :bonus_amount       => 0,
-                        :bonus_type         => "",
-                        :bonus_to           => "",
-                        :bonus_max          => 0,
-                        :duration           => 0,
-                        :lore_used          => Skills.type_of_lore,
-                        :repetions          => 999,
-                        :repeat_modifier    => 0
-                    }
-                },
+        000 => {
+            "effect" => {
+                :description        => "",
+                :chart              => chart_function(),
+                :bonus_amount       => 0,
+                :bonus_type         => "",
+                :bonus_to           => "",
+                :bonus_max          => 0,
+                :duration           => 0,
+                :lore_used          => Skills.type_of_lore,
+                :repetions          => 999,
+                :repeat_modifier    => 0
+            }
+        },
 =end
 
 module Lich
@@ -63,15 +61,14 @@ module Lich
         # TODO   value is string array of #s for lore_used
       end
 
-      def self.per_x_ranks_chart(per_rank,starting_rank,ending_rank)
-        #TODO  Increase is given every value ranks.
+      def self.per_x_ranks_chart(per_rank, starting_rank, ending_rank)
+        # TODO  Increase is given every value ranks.
         # use 999 for no ending rank
       end
 
       def self.generic_chart(value)
-        #TODO  unspecified bonus
+        # TODO  unspecified bonus
       end
-
     end
   end
-end 
+end
